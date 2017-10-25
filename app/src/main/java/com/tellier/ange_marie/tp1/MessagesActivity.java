cc931e6831f70b85b2fba86ba162923ee1edc3a3
angemarie <angem.ifor@gmail.com>
2017-10-25 14:15:49 +0200

    Envoie de messages
    Ajout de l'envoie de message

--- app/src/main/java/com/tellier/ange_marie/tp1/MessagesActivity.java
+++ app/src/main/java/com/tellier/ange_marie/tp1/MessagesActivity.java
@@ -5,8 +5,11 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tellier.ange_marie.tp1.adapter.MessagesAdapter;
 import com.tellier.ange_marie.tp1.helper.NetworkHelper;
@@ -15,7 +18,9 @@ import com.tellier.ange_marie.tp1.model.Message;
 
 import org.json.JSONException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MessagesActivity extends AppCompatActivity {
     MessagesAdapter adapter;
@@ -24,13 +29,24 @@ public class MessagesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
-
+        final EditText msg = (EditText) findViewById(R.id.newmsg);
         ListView list = (ListView) findViewById(R.id.messages);
 
         adapter = new MessagesAdapter(MessagesActivity.this);
         list.setAdapter(adapter);
-
         token = this.getIntent().getExtras().getString("token");
+
+        if(token == null) {
+            Toast.makeText(MessagesActivity.this, "Token invalide", Toast.LENGTH_SHORT).show();
+            MessagesActivity.this.finish();
+        }
+
+        findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new SendMsgAsyncTask(MessagesActivity.this).execute(msg.getText().toString());
+            }
+        });
 
         new GetMessageAsyncTask(MessagesActivity.this).execute();
 
@@ -80,4 +96,37 @@ public class MessagesActivity extends AppCompatActivity {
    }
 
 
+    public class SendMsgAsyncTask extends AsyncTask<String, Void, Http_result> {
+        Context c;
+
+        public SendMsgAsyncTask(Context c){
+            this.c = c;
+        }
+
+        protected Http_result doInBackground(String... strings){
+            if(!NetworkHelper.isInternetAvailable(c)){
+                return null;
+            }
+
+            try {
+                Map<String, String > map = new HashMap();
+                map.put("message", strings[0]);
+                Http_result r = NetworkHelper.doPost("http://cesi.cleverapps.io/messages", map, token);
+
+                return r;
+            }catch (Exception e){
+                Log.e("NetworkHelper", e.getMessage());
+                return null;
+            }
+        }
+
+        protected void onPostExecute(Http_result r){
+            if(r.status != 200){
+                Toast.makeText(c, "Message non envoyé", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(c, "Message envoyé", Toast.LENGTH_SHORT).show();
+                new GetMessageAsyncTask(c).execute();
+            }
+        }
+    }
 }
